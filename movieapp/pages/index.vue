49d885df8a82dedcaedd96cb6aef44530c0ba31a
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-05-23 17:05:22 +0200

    call de l'api tmdb et affichage de données dans index

--- movieapp/pages/index.vue
+++ movieapp/pages/index.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="home">
-    
+    <!-- HERO -->
+    <Hero />
+
+    <!-- Movies -->
+    <div class="container movie">
+      <div id="movie-grid" class="movie-grid">
+        <div v-for="(movie, index) in movies" :key="index" class="movie">
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/${movie.poster_path}`" alt="" />
+            <p class="review">{{movie.vote_average}}</p>
+            <p class="overview">{{movie.overview}}</p>
+
+            
+            <!-- ON EST ACTUELLEMENT ICI -->
+
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch(){
+    await this.getMovies()
+  },
+  methods: {
+   async getMovies(){
+     const data = axios.get(
+       `https://api.themoviedb.org/3/movie/now_playing?api_key=583a93ce4dd56c8af08dcfe52409369f&language=en-US&page=1`
+     );
+     const result = await data;
+     result.data.results.forEach(movie => {
+       this.movies.push(movie);
+     });
+   },
+  },
 }
 </script>
